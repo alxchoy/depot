e20c1f5d6a9918d7eb04d5dd3bf4bd0da2748e56
Choy <alexanderchoy18@gmail.com>
2017-06-21 01:07:02 -0500

    Validación de formulario para la creación de un producto

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+	validates :title, :description, :image_url, presence: true # sirve para validar cada campo de nuestro formulario, antes de ingresarlo a la DB
+	validates :price, numericality: { greater_than_or_equal_to: 0.01 } # verifica un precio valido
+	validates :title, uniqueness: true # validamos con el titulo del producto sea unico
+	# validamos la url de la imagen, que pueda hacer gif, png o jpg (con format comparamos un campo con una expresion regular) y allow_blank permite dejarlo en blanco
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message: 'must be a URL for GIF, JPG or PNG image'
+	}
 end
